198446d556e59d0f21a9e155b485e3b953f3ab51
ta_wuhen <781739081@qq.com>
2015-08-27 23:19:41 +0800

    add role,user and so

--- NoTrace/NoTrace.Core/Authorization/PermissionChecker.cs
+++ NoTrace/NoTrace.Core/Authorization/PermissionChecker.cs
@@ -1,9 +1,14 @@
 ﻿using Abp.Authorization;
+using Abp.Authorization.Users;
+using NoTrace.Core.MultiTenancy;
+using NoTrace.Core.Users;
 
 namespace NoTrace.Core.Authorization
 {
-    public class PermissionChecker: PermissionChecker<Tenant, Role, User>
+    public class PermissionChecker:PermissionChecker<Tenant,Role,User>
     {
-         
+        public PermissionChecker(AbpUserManager<Tenant, Role, User> userManager) : base(userManager)
+        {
+        }
     }
 }
---
+++ NoTrace/NoTrace.Core/Authorization/Role.cs
@@ -0,0 +1,11 @@
+﻿using Abp.Authorization.Roles;
+using NoTrace.Core.MultiTenancy;
+using NoTrace.Core.Users;
+
+namespace NoTrace.Core.Authorization
+{
+    public class Role:AbpRole<Tenant,User>
+    {
+         
+    }
+}
